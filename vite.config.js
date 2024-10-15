9a1d7f3bb933dc8310cb931ed8041386fe1c4de7
ramyhaj05 <jpalmodovar@santarosa.com>
2024-10-15 15:40:35 +0800

    try sa github

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import staticBase from 'vite-plugin-static-base';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ramyhaj05/'
 })
